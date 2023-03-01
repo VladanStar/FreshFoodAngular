c889775cde2a123bddebd91bfbf6f54f475a91bc
VladanStar <vladancupric@gmail.com>
2023-03-01 09:27:10 +0100

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-
+import { MenupageComponent } from './pages/menupage/menupage.component';
 const routes: Routes = [
 {path:'', component:HomeComponent},
 {path:'menu' ,component:MenuComponent},
+{path:'menupage/:id', component:MenupageComponent},
 {path:'about', component:AboutComponent},
 {path:'contact',component: ContactComponent},
 
